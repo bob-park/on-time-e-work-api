229ba324bd52483a7b18ccebafe464d3ef64098e
Bob Park <hwpark11@kakao.com>
2025-04-21 22:21:02 +0900

    feat: 휴일근무보고서 table ddl 추가

---
+++ src/main/resources/db/migration/V0.0.001_2025_0421__add-overtime-work.sql
@@ -0,0 +1,34 @@
+-- 휴일 근무 보고서
+create table documents_overtime_works
+(
+    id                 bigserial               not null primary key,
+    created_date       timestamp default now() not null,
+    created_by         varchar(50)             not null,
+    last_modified_date timestamp,
+    last_modified_by   varchar(50)
+);
+
+create table overtime_works_times
+(
+    id               bigserial                   not null primary key,
+    document_id      bigint                      not null,
+    start_date       timestamp                   not null,
+    end_date         timestamp                   not null,
+    applied_hours    numeric(8, 4) default 0     not null,
+    user_unique_id   varchar(41)                 not null,
+    contents         varchar(250)                not null,
+    is_day_off       bool          default false not null,
+    is_extra_payment bool          default false not null,
+
+    foreign key (document_id) references documents_overtime_works (id)
+);
+
+create table overtime_works_times_applied_hour_reports
+(
+    id      bigserial not null primary key,
+    time_id bigint    not null,
+    report  text      not null,
+
+    foreign key (time_id) references overtime_works_times (id)
+);
+
