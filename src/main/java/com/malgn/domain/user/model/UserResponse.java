ed9fa15ae833a7c887b9afaaa496e73ee5510566
Bob Park <hwpark@malgn.com>
2025-08-05 16:58:19 +0900

    refactor: 인증 서버 모듈 변경 적용

--- src/main/java/com/malgn/domain/user/model/UserResponse.java
+++ src/main/java/com/malgn/domain/user/model/UserResponse.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import com.malgn.domain.team.model.TeamResponse;
 
 @Builder
-public record UserResponse(String uniqueId,
+public record UserResponse(String id,
                            String userId,
                            String username,
                            TeamResponse team,
